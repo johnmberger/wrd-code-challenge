da597749b4aa43910168e57c3ae31aabc69555cc
johnmberger <johnmartinberger@gmail.com>
2016-10-30 12:07:41 -0600

    file upload wired up

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
-// jquery test
-$(document).on('ready', () => {
-  console.log('sanity check!');
-});
+(function(){
 
-// es6 test
-const PI = 3.14;
+    function onChange(event) {
+      var reader = new FileReader();
+      reader.onload = onReaderLoad;
+      reader.readAsText(event.target.files[0]);
+    }
 
-// mocha test
-function sum(num1, num2) {
-  return parseInt(num1) + parseInt(num2);
-}
+    function onReaderLoad(event){
+      var obj = JSON.parse(event.target.result);
+      console.log(obj);
+      $('#target').html('this works!');
+    }
+
+    document.getElementById('upload').addEventListener('change', onChange);
+
+}());
