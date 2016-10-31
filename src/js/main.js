a8f504c94660daa29095236b70f566e7630870e4
johnmberger <johnmartinberger@gmail.com>
2016-10-31 13:34:18 -0600

    got test2 working... seems a little hacky though

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 
   $('#upload').on('change', function(e) {
 
+    $('#target').empty();
     var file = e.target.files[0];
     var reader = new FileReader();
 
@@ -10,10 +11,31 @@
 
         let parsedJSON = JSON.parse(e.target.result);
 
-        parsedJSON.forEach(item => {
-          $('#target').append(`<${item.tag}>${item.content}</${item.tag}>`);
-        });
+        parsedJSON.forEach((parent, i) => {
 
+          $('#target').append(`<${parent.tag}></${parent.tag}>`);
+
+          let childContent = parent.content.content;
+          let childTag = parent.content.tag;
+
+          // *** weird js typecheck *** //
+          // actually checking for array of objects //
+          if (typeof childContent === 'object') {
+
+            childContent.forEach(child => {
+
+              $(`${parent.tag}:nth-child(${i+1})`)
+              .append(`<${child.tag}>${child.content}</${child.tag}>`);
+
+            });
+
+          } else {
+
+            $(`${parent.tag}`)
+            .append(`<${childTag}>${childContent}</${childTag}>`);
+
+          }
+        });
       };
     })(file);
 
